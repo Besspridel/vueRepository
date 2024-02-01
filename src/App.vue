8eec0cd41775f00650060cd2e3c8f71489474d48
Nikita <aurora.02@mail.ru>
2024-02-01 15:19:39 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <user-form @form-submitted="updateUserInfo" />
+    <user-info :userInfo="userInfo" />
+  </div>
+</template>
+
+<script>
+import UserForm from './components/UserForm.vue';
+import UserInfo from './components/UserInfo.vue';
+
+export default {
+  components: {
+    UserForm,
+    UserInfo
+  },
+  data() {
+    return {
+      userInfo: {}
+    };
+  },
+  methods: {
+    updateUserInfo(userData) {
+      this.userInfo = userData;
+    }
+  }
+};
+</script>
+
+<style>
+/* Добавьте общие стили для вашего приложения, если необходимо */
+</style>
